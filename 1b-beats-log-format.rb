8f29b1ca1cddb415888d5913463f80c8d3f72a11
DhanashriJoshi <dhanashrijoshi91@gmail.com>
2019-08-20 16:23:53 +0530

    Filebeat Logstash Protobuf

---
+++ 1b-beats-log-format.rb
@@ -0,0 +1,44 @@
+{
+  "host" => {
+    "id" => "ce6b0fcce8894499b8d28e4ca4855cc9",
+    "containerized" => false,
+    "hostname" => "bhavesh",
+    "os" => {
+      "platform" => "ubuntu",
+      "family" => "debian",
+      "version" => "16.04.6 LTS (Xenial Xerus)",
+      "codename" => "xenial",
+      "kernel" => "4.15.0-55-generic",
+      "name" => "Ubuntu"
+    },
+    "architecture" => "x86_64",
+    "name" => "bhavesh"
+  },
+  "log" => {
+    "file" => {
+      "path" => "/home/bhavesh/test.log"
+    },
+    "offset" => 0
+  },
+  "message" => "Log message will appear here",
+  "agent" => {
+    "id" => "4b5584a4-e0e3-49e3-acaf-70a0d0b2d5df",
+    "version" => "7.3.0",
+    "ephemeral_id" => "aa41a09e-16d6-44d9-b724-d9828623e6fd",
+    "type" => "filebeat",
+    "hostname" => "bhavesh"
+  },
+  "@version" => "1",
+  "@timestamp" => 2019 - 08 - 18 T13: 47: 42.649 Z,
+    "input" => {
+      "type" => "log"
+    },
+    "ecs" => {
+      "version" => "1.0.1"
+    },
+    "tags" => [
+      [0]
+      "beats_input_codec_plain_applied"
+    ],
+    "type" => "beats"
+}
---
+++ 2b-rsyslog-log-format.rb
@@ -0,0 +1,15 @@
+{
+  "message" => "<30>Aug 18 19:10:24 bhavesh logstash[2913]: Thread.exclusive is deprecated, use Thread::Mutex\n",
+  "severity_label" => "Emergency",
+  "severity" => 0,
+  "facility" => 0,
+  "tags" => [
+    [0] "_grokparsefailure_sysloginput"
+  ],
+  "@version" => "1",
+  "facility_label" => "kernel",
+  "priority" => 0,
+  "host" => "127.0.0.1",
+  "type" => "syslog",
+  "@timestamp" => 2019-08-18T13:40:24.980Z
+}
